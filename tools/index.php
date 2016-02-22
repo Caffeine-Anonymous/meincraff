f9345044b1dd9522e9a58168908cb9bc770d8df6
Nick Sargente <primis@primiscomputers.com>
2016-02-22 14:55:42 -0500

    Initial commit

---
+++ tools/index.php
@@ -0,0 +1,21 @@
+<html>
+<body>
+<h2>
+Users Currently Online
+</h2>
+<hr>
+<?php
+$output = shell_exec("ssh minecraft@notch '/usr/local/etc/rc.d/minecraft command list'");
+echo "<pre>$output</pre>";
+?>
+<h2>
+Users Online Today
+</h2>
+<hr>
+<?php 
+$output = shell_exec("ssh minecraft@notch 'cat logs/latest.log' | grep UUID");
+echo "<pre>$output</pre>";
+?>
+</body>
+</html>
+
