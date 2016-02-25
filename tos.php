378f15f0692c75cb30907431e1303850395a1854
Nick Sargente <primis@primiscomputers.com>
2016-02-24 21:05:59 -0500

    Terms of Service file. Will soon be .html

---
+++ tos.php
@@ -0,0 +1,177 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" 
+    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title> TasteyServ Minecraft Terms of Service</title>
+</head>
+<body>
+
+<h1>TOS</h1>
+
+<h2>Introduction</h2>
+<p id="intro">
+    Welcome to TasteyServ. <br />
+    We have some rules you must follow here. Please read them all.
+</p>
+
+<h2>Basic Server Rules</h2>
+
+<h3>Don't be a dick.</h3>
+<p id="dick">
+    Honestly this is the most important rule. If you're a dick, no one will
+    like you and we'll probably ban you. Minecraft is about having fun with
+    friends. If you don't want to be our friend, then we don't need to deal
+    with you.
+</p>
+
+<h3>No Griefing.</h3>
+<p id="greifing">
+    This should be pretty obvious. If we catch you griefing, we will ban you,
+    and submit your username to mcbans. 
+    We'll also sign your email up for random spam mail just to get back at you.
+</p>
+
+<h3>Language</h3>
+<p id="language">
+    You can curse, but if we ask you to tone it down you should probably listen.
+</p>
+
+<h3>Trolls</h3>
+<p id="trolling">
+    Trolling occasionally is funny. Flaming is not. 
+    CAPS LOCK IS CRUISE CONTROL ON THE ROAD TO BEING KICKED.
+</p>
+
+<h3>Asking for OP</h3>
+<p id="can-has-op">
+    If you have to ask for op, you already know the answer is no. 
+    If you actually need additional permissions for things, 
+    just ask for the permission from an op. Or better yet, ask an op to do it
+    for you.
+</p>
+
+<h2>Build rules</h2>
+
+<h3>TNT</h3>
+<p id="tnt">
+    TNT is not banned, It's not deactivated, because there are legitimate 
+    uses for it. However, excessive use of TNT is discouraged,
+    because destruction of the landscape is still considered greifing.
+</p>
+
+<h3>Survival Maps</h3>
+<p id="survival">
+    Survival worlds are SURVIVAL only.
+    Also, clear your damn inventory when you switch worlds
+</p>
+
+<h3>Animal Spawning</h3>
+<p id="animals">
+    Spawning animals and mobs is fine. 
+    Spawning a bunch of them in one location will crash the server, 
+    which is not fine. Therefore making an animal spawner which 
+    intentionally does this is a bannable offense.
+</p>
+
+<h3>Clipboard Clearing</h3>
+<p id="clipboard">
+    Dunjin realm is not for worldedit clipboard clearing,
+    contrary to popular belief. If you have a world edit clipboard you
+    want to keep, find a legitimate home for it.
+</p>
+
+<h3>Towers and Tunnels</h3>
+<p id="towering">
+    No 1x1 towers or 2x1 tunnels.
+    They look like crap and you shouldn't need to use them 
+    since the overworld is creative. You're not allowed to do them in
+    Survival either.
+</p>
+
+<h3>Pixel Art</h3>
+<p id="pixel-art">
+    No Pixel art. There are plenty of servers for this,
+    We are not one of them. 
+</p>
+
+<h2>City Planning</h2>
+
+<h3>Rails</h3>
+<p id="rails">
+    We have rail systems that span the entire map, 
+    if you build a good city, we will link you to one of them.
+    If you want to build your own rail system, that's cool too,
+    just let us know before you decide to go "nationwide"
+    so we can plan out an easement for you.
+</p>
+
+<h3>Structural Support</h3>
+<p id="structures">
+    Structures should be built logically and with structural support.
+    Unless you're planning on making a flying city, there should be a 
+    foundation, supports, archetectual considerations, and aesthetic
+    appeal in your builds. This applies on all maps on the server.
+    If your builds look terrible (made from stone/dirt/etc) or is built
+    inside someone else's city without permission, your build may be moved
+    or removed to keep the server looking nice. 
+</p>
+
+<h3>New Cities</h3>
+<p id="cities">
+    If you want to build a city, let us help you choose a location.
+    We have tons of space but city planning is essential into keeping
+    the map organized. You may not realize just how close you are to 
+    someone else's stuff until you're building on top of it.
+</p>
+
+<h4>City features</h4>
+<p id="city-features">
+    Cities have boaders, names, and points of interest.
+    Your city doesn't have to be a grid, but having plots availible for
+    public build projects is a good idea. Having a welcome center or Town
+    Hall is generally a good idea as well, since people will eventually wonder
+    what your city is when you're not online.
+</p>
+<h3>Building in Cities</h3>
+<p id="city-building">
+    Feel free to help us on city builds, just remember that even though 
+    most of them are community built, you should still ask before 
+    modifying existing structures or taking an empty lot inside a city's walls.
+</p>
+<h2>Notice of Change</h2>
+<p id="change">
+    Please note that this TOS may change at any time,
+    and it is expected that you re-read it every now and again.
+</p>
+<h2>User Acceptance and Submission</h2>
+<p id="affirmation">
+    By Clicking the Checkbox below and entering my username and email, I agree
+    to the terms of service of this minecraft server listed above. 
+    I also affirm that my email address is valid and give the administrators of
+    this server to occasionally send me a newsletter regarding any server 
+    events or activities.<br> 
+    After submitting this form you will be added to the
+    "member" class of users on the server.
+    This form will not work unless you have logged into the server before, so
+    you must log in before hitting submit or the submission will not succeed.
+</p>
+<form action="submit-tos.php" method="post">
+<p id="submission">
+<input type="checkbox" name="agree" value="yes">
+I agree to the Terms of Service above and that my information is correct.<br\>
+Username: <input type="">
+
+<input type="submit" value="Submit">
+</p>
+</form>
+<hr />
+<div id="footer">
+    <a href="http://validator.w3.org/check?uri=referer">
+        <img src="http://www.w3.org/Icons/valid-xhtml10"
+            alt="Valid XHTML 1.0 Strict" height="31" width="88" />
+    </a>
+</div>
+</body>
+</html>
