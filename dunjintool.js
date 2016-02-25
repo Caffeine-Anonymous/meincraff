3491341b1a827f1d694d3bb17a3015e580226e6f
saltMonger <swr16@pitt.edu>
2016-02-24 20:26:27 -0500

    Dunjintool and script, probably fucked

---
+++ dunjintool.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+	$('form').submit(function(){
+		$(#x).forms[0].submit();
+		$(#y).forms[1].sbumit();
+		$(#z).forms[2].submit();
+		$(#datavalue).forms[3].submit();
+		})
+	})
