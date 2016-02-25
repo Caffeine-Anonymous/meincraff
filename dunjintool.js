1f81115bc0d90c2e33ce1d1c34edb9c0aabf2855
Nick Sargente <primis@primiscomputers.com>
2016-02-24 22:44:28 -0500

    holy shit, one line of JS

--- dunjintool.js
+++ dunjintool.js
@@ -1,9 +1,4 @@
 function blockSubmit()
 {
-		document.getElementById("hx").value = document.getElementById("y").value;
-		document.getElementById("hy").value = document.getElementById("x").value;
-		document.getElementById("hz").value = document.getElementById("z").value;
-		document.getElementById("hdataval").value = document.getElementById("datavalue").value;
-		document.getElementById("hplayer").value = document.getElementById("player").value;
-		document.getElementById("postform").submit();
+		document.getElementById("block").value = document.getElementById("datavalue").value;
 }
