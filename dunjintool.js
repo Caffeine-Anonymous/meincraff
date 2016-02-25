4ba6a2bab981b83dddb29b42ea96167c22879e74
saltMonger <swr16@pitt.edu>
2016-02-24 22:04:13 -0500

    Switched the thing

--- dunjintool.js
+++ dunjintool.js
@@ -1,11 +1,25 @@
 $(document).ready(function(){
 	$('form').submit(function(){
-		document.getElementById("hx").value = document.getElementById("x").value;
-		document.getElementById("hy").value = document.getElementById("y").value;
-		document.getElementById("hz").value = document.getElementById("z").value;
-		document.getElementById("hdataval").value = document.getElementById("datavalue").value;
-		document.getElementById("hplayer").value = document.getElementById("player").value;
+		getUrlVars();
+		document.getElementById("hx").value = vars[0];
+		document.getElementById("hy").value = vars[1];
+		document.getElementById("hz").value = vars[2];
+		document.getElementById("hdataval").value = vars[3];
+		document.getElementById("hplayer").value = vars[4];
 		document.getElementById("postform").submit();
 		})
 	})
+
+function getUrlVars()
+{
+    var vars = [], hash;
+    var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+    for(var i = 0; i < hashes.length; i++)
+    {
+        hash = hashes[i].split('=');
+        vars.push(hash[0]);
+        vars[hash[0]] = hash[1];
+    }
+    return vars;
+}
 	
