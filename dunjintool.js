fd452907d7ad6239cad4f25c09e8c231ad639207
saltMonger <swr16@pitt.edu>
2016-02-24 21:45:47 -0500

    Fuck.

--- dunjintool.js
+++ dunjintool.js
@@ -1,10 +1,11 @@
 $(document).ready(function(){
 	$('form').submit(function(){
 		document.getElementById("hx").value = document.getElementById("x").value;
-		document.getElementByID("hy").value = document.getElementById("y").value;
-		document.getElementByID("hz").value = document.getElementById("z").value;
-		document.getElementByID("hdataval").value = document.getElementById("datavalue").value;
-		document.getElementByID("hplayer").value = document.getElementById("player").value;
+		document.getElementById("hy").value = document.getElementById("y").value;
+		document.getElementById("hz").value = document.getElementById("z").value;
+		document.getElementById("hdataval").value = document.getElementById("datavalue").value;
+		document.getElementById("hplayer").value = document.getElementById("player").value;
+		document.getElementById("postform").submit();
 		})
 	})
 	
