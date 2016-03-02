19a98820d277fd33f443c4df53937cdcbf467046
Nick Sargente <primis@primiscomputers.com>
2016-03-02 01:44:40 -0500

    Didn't fix php short open tag completely... Whoops

--- index.php
+++ index.php
@@ -8,19 +8,19 @@
 <html lang="en" xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title> <?php=$server_name?> </title>
+<title> <?php echo $server_name;?> </title>
 </head>
     <body>
     <?php if(isset($votey_id)): ?>
     <p id="votey">
-        <a href="http://minecraftservers.net/server/<?php=$votey_id?>/">
-        <img src="http://minecraftservers.net/status/server_<?php=$votey_id?>.png"
+        <a href="http://minecraftservers.net/server/<?php echo $votey_id;?>/">
+        <img src="http://minecraftservers.net/status/server_<?php echo $votey_id;?>.png"
         alt="Vote for our server!" style="float: right" />
         </a>
     </p>
     <?php endif; ?>
 
-    <h1> <?php=$server_name?> </h1>
+    <h1> <?php echo $server_name;?> </h1>
     <p id="navbar">
         <?php include("navbar.html")?>
     </p>
@@ -47,14 +47,15 @@ catch (Exception $e) {
 <?php foreach ($users as $user): ?>
     <td style='text-align:center'>
     <!--Thank you to minotar for your awesome head API-->
-    <img src="http://minotar.net/helm/<?php=$user?>/45" alt="<?php=$user?>"/>
+        <img src="http://minotar.net/helm/<?php echo $user;?>/45" 
+            alt="<?php echo $user;?>" />
     </td>
 <?php endforeach; ?>
 </tr>
 
 <tr>
 <?php foreach ($users as $user): ?>
-    <td style='text-align:center'> <?php=$user?> </td>
+    <td style='text-align:center'> <?php echo $user;?> </td>
 <?php endforeach; ?>
 </tr>
 </table>
